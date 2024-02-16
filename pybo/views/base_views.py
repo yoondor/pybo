73da49c48c31b5dab2c9d596daba6967bd9c56e1
5yoondori <5yoondori@snu.ac.kr>
2024-02-16 22:32:46 +0900

    로깅 완료

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,8 +4,11 @@ from django.db.models import Q
 
 from ..models import Question
 
+import logging
+logger = logging.getLogger('pybo')
+
 def index(request):
-    3/0
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
